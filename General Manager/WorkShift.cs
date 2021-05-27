fb39811fa7652a58df43ec6c621d1ffd554bebe8
foxthehuman <80444358+foxthehuman@users.noreply.github.com>
2021-05-27 13:47:13 +0700

    Revert "Thien nguyen"

--- General Manager/WorkShift.cs
+++ General Manager/WorkShift.cs
@@ -27,141 +27,43 @@ namespace General_Manager.Form
                 return false;
             }
         }
-        public DataTable Show()
-        {
-            Database data = new Database();
-            SqlCommand command = new SqlCommand();
-            command.Connection = data.GetConnection;
-            command.CommandText = "Select id from Schedule";
-            SqlDataAdapter SDA = new SqlDataAdapter(command);
-            DataTable table = new DataTable();
-            SDA.Fill(table);
-            return table;
-        }
-        public int GetIdEmployee()
-        {
-            DataTable table = this.Show();
-            EmployeeHotel employee = new EmployeeHotel();
-            int[] ID = new int[20];
-            int id;
-            Random rand = new Random();
-            int total = Convert.ToInt32(employee.GetNumberJanior()) + Convert.ToInt32(employee.GetNumberOfManager()) + Convert.ToInt32(employee.GetNumberReceptionist());
-            for (int i = 0; i < total; i++)
-            {
-                id = Convert.ToInt32(table.Rows[i]["id"].ToString().Trim());
-                ID[i] = id;               
-            }
-            int index = rand.Next(ID.Length);
-            return ID[index];
-        }
         public void GetShift()
         {
-            //EmployeeHotel employee = new EmployeeHotel();
-            //int manager = Convert.ToInt32(employee.GetNumberOfManager());
-            //int receptionist = Convert.ToInt32(employee.GetNumberReceptionist());
-            //int janior = Convert.ToInt32(employee.GetNumberJanior());
-            Random ran = new Random();
-            for (int i = 0; i < 20; i++)
+            EmployeeHotel employee = new EmployeeHotel();
+            int manager = Convert.ToInt32(employee.GetNumberOfManager());
+            int receptionist = Convert.ToInt32(employee.GetNumberReceptionist());
+            int janior = Convert.ToInt32(employee.GetNumberJanior());
+            for (int i = 0; i < manager; i++)
             {
-                int ch1 = ran.Next(1, 4);
-                int ch2 = ran.Next(1, 4);
-                int ch3 = ran.Next(1, 4);
-                int ch4 = ran.Next(1, 4);
-                int ch5 = ran.Next(1, 4);
-                int ch6 = ran.Next(1, 4);
-                int ch7 = ran.Next(1, 4);
-                int id = this.GetIdEmployee();
-                this.UppdateShift(ch1, ch2, ch3, ch4, ch5, ch6, ch7, id);
+                this.InsertShift("OK");
             }
         }
-        public bool DeleteData()
+        public bool InsertShift(string shift)
         {
             Database data = new Database();
-            SqlCommand command = new SqlCommand("delete from Schedule", data.GetConnection);
+            SqlCommand command = new SqlCommand("INSERT INTO Schedule (monday) VALUES (@s)", data.GetConnection);
+            command.Parameters.Add("@s", SqlDbType.NVarChar).Value = shift;
             data.OpenConnection();
             if (command.ExecuteNonQuery() > 0)
             {
-                data.CloseConnection();
                 return true;
             }
             else
             {
-                data.CloseConnection();
-                return false;
-            }
-        }
-        public bool InsertShift()
-        {
-            Database data = new Database();
-            SqlCommand command = new SqlCommand("insert into Schedule (Id, firstname, lastname, role) select Id, fname, lname, role from Employee", data.GetConnection);
-            data.OpenConnection();
-            if (command.ExecuteNonQuery() > 0)
-            {
-                data.CloseConnection();
-                return true;
-            }
-            else
-            {
-                data.CloseConnection();
-                return false;
-            }
-        }
-        public bool UppdateShift(int shiftmon, int shifttu, int shiftwed, int shiftthus, int shiftfri, int shiftsat, int shiftsun, int id)
-        {
-            Database data = new Database();
-            SqlCommand command = new SqlCommand("Update Schedule SET monday = @mon, tuesday = @tu, wednesday = @wed, thusday = @thus, friday = @fri, satusday = @sat, sunday = @sun Where Id = @id", data.GetConnection);
-            command.Parameters.Add("@mon", SqlDbType.Int).Value = shiftmon;
-            command.Parameters.Add("@tu", SqlDbType.Int).Value = shifttu;
-            command.Parameters.Add("@wed", SqlDbType.Int).Value = shiftwed;
-            command.Parameters.Add("@thus", SqlDbType.Int).Value = shiftthus;
-            command.Parameters.Add("@fri", SqlDbType.Int).Value = shiftfri;
-            command.Parameters.Add("@sat", SqlDbType.Int).Value = shiftsat;
-            command.Parameters.Add("@sun", SqlDbType.Int).Value = shiftsun;
-            command.Parameters.Add("@id", SqlDbType.Int).Value = id;
-            data.OpenConnection();
-            if(command.ExecuteNonQuery() > 0)
-            {
-                data.CloseConnection();
-                return true;
-            }
-            else
-            {
-                data.CloseConnection();
                 return false;
             }
         }
         public DataTable ShowShift()
         {
+            this.GetShift();
             Database data = new Database();
             SqlCommand command = new SqlCommand();
-            data.OpenConnection();
             command.Connection = data.GetConnection;
-            command.CommandText = "Select id, firstname, lastname, role, monday, tuesday, wednesday, thusday, friday, satusday, sunday from Schedule";
+            command.CommandText = "Select id, firstname, lastname, monday from Schedule";
             SqlDataAdapter SDA = new SqlDataAdapter(command);
             DataTable table = new DataTable();
             SDA.Fill(table);
             return table;
-        }
-        public DataTable ShowReloadShift()
-        {
-            DataTable table = new DataTable();
-            Database data = new Database();
-            try
-            {
-                this.GetShift();
-                SqlCommand command = new SqlCommand();
-                data.OpenConnection();
-                command.Connection = data.GetConnection;
-                command.CommandText = "Select id, firstname, lastname, role, monday, tuesday, wednesday, thusday, friday, satusday, sunday from Schedule";
-                SqlDataAdapter SDA = new SqlDataAdapter(command);
-                SDA.Fill(table);
-                return table;
-            }
-            catch
-            {
-                return table;
-            }
-
         }
         public DataTable WorkTime(int Id)
         {
